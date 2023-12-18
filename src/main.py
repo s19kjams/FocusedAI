1305576007496ad6ab3f1047695c5fe3bf2ad00e
KiavashJamshidi <kiavashjamshidi@live.com>
2023-12-18 01:52:30 +0100

    implemented delete cache

--- src/main.py
+++ src/main.py
@@ -20,6 +20,11 @@ def get_cache(key):
 def set_cache(key, value):
     cache[key] = value
 
+def delete_cache(key):
+    if key in cache:
+        del cache[key]
+
+
 def get_db():
     db = SessionLocal()
     try:
@@ -29,7 +34,10 @@ def get_db():
 
 @app.post("/courses/", response_model=Course)
 def create_course(course: CourseCreate, db: Session = Depends(get_db)):
-    return add_course(db=db, course=course)
+    new_course = add_course(db=db, course=course)
+    cache_key = hashkey("courses")
+    delete_cache(cache_key)
+    return new_course
 
 @app.get("/courses/", response_model=list[Course])
 def get_courses(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
@@ -47,7 +55,10 @@ def get_courses(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
 @app.post("/lessons/", response_model=Lesson)
 def create_lesson(lesson: LessonCreate, db: Session = Depends(get_db)):
-    return add_lesson(db=db, lesson=lesson)
+    new_lesson = add_lesson(db=db, lesson=lesson)
+    cache_key = hashkey("lessons")
+    delete_cache(cache_key)
+    return new_lesson
 
 
 @app.get("/lessons/", response_model=list[Lesson])
@@ -66,7 +77,10 @@ def get_lessons(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
 @app.post("/students/", response_model=Student)
 def create_student(student: StudentCreate, db: Session = Depends(get_db)):
-    return add_student(db=db, student=student)
+    new_student = add_student(db=db, student=student)
+    cache_key = hashkey("students")
+    delete_cache(cache_key)
+    return new_student
 
 
 @app.get("/students/", response_model=list[Student])
@@ -84,7 +98,10 @@ def get_students(skip: int = 0, limit: int = 100, db: Session = Depends(get_db))
 
 @app.post("/teachers/", response_model=Teacher)
 def create_teacher(teacher: TeacherCreate, db: Session = Depends(get_db)):
-    return add_teacher(db=db, teacher=teacher)
+    new_teacher = add_teacher(db=db, teacher=teacher)
+    cache_key = hashkey("teachers")
+    delete_cache(cache_key)
+    return new_teacher
 
 
 @app.get("/teachers/", response_model=list[Teacher])
@@ -102,12 +119,19 @@ def get_teachers(skip: int = 0, limit: int = 100, db: Session = Depends(get_db))
 
 @app.post("/enrollments/")
 def enroll_student(enrollment: EnrollmentCreate, db: Session = Depends(get_db)):
-    return add_enrollment(db=db, enrollment=enrollment)
+    new_enrollment = add_enrollment(db=db, enrollment=enrollment)
+    cache_key = hashkey("enrollments")
+    delete_cache(cache_key)
+    return new_enrollment
 
 
 @app.delete("/enrollments/{student_id}/{course_id}")
 def disenroll_student(student_id: int, course_id: int, db: Session = Depends(get_db)):
-    return remove_enrollment(db=db, student_id=student_id, course_id=course_id)
+    removed_enrollment = remove_enrollment(db=db, student_id=student_id, course_id=course_id)
+    
+    cache_key = hashkey("enrollments")
+    delete_cache(cache_key)
+    return removed_enrollment
 
 
 @app.get("/enrollments/", response_model=list[Enrollment])
